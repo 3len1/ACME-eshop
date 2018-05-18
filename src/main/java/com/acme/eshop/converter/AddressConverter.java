7da338d9acf9885565d7f2b2fbf1b021190af83e
Theodoros Karropoulos <tkarropoulos@gmail.com>
2018-05-19 00:04:05 +0300

    ...

--- src/main/java/com/acme/eshop/converter/AddressConverter.java
+++ src/main/java/com/acme/eshop/converter/AddressConverter.java
@@ -3,12 +3,14 @@ package com.acme.eshop.converter;
 import com.acme.eshop.domain.Address;
 import com.acme.eshop.dto.AddressDto;
 import javaslang.control.Try;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Eleni on 16/5/2018.
  */
 @Component
+
 public class AddressConverter {
 
     public Address getAddress(AddressDto addressDto) {
