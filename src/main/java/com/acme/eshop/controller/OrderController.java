6b4ef8be6c9766a3dab5ea1d474fa9d6b7375e27
Theodoros Karropoulos <tkarropoulos@gmail.com>
2018-05-21 12:54:07 +0300

    Finish OrderController

--- src/main/java/com/acme/eshop/controller/OrderController.java
+++ src/main/java/com/acme/eshop/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.acme.eshop.controller;
 
 import com.acme.eshop.domain.Item;
 import com.acme.eshop.domain.Order;
+import com.acme.eshop.resources.ItemResource;
+import com.acme.eshop.resources.OrderResource;
 import com.acme.eshop.service.LoginService;
 import com.acme.eshop.service.OrderService;
 import io.swagger.annotations.Api;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -79,5 +82,80 @@ public class OrderController {
                 .body(orderService.getAllItemsFromOrder(orderCode, loginService.getUser(sessionID).getId()));
     }
 
+    @ApiOperation(value = "Create an Order")
+    @RequestMapping(value = "/orders", method = RequestMethod.POST)
+    public ResponseEntity<Order> createOrder(@Valid @RequestBody OrderResource order, @RequestHeader("sessionID") UUID sessionID) {
+        if(loginService.getUser(sessionID) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
 
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(orderService.createOrder(order, loginService.getUser(sessionID).getId()));
+
+    }
+
+    @ApiOperation(value = "Delete an Order")
+    @RequestMapping(value = "/orders/{orderCode}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteOrder(@RequestHeader("sessionID") UUID sessionID, @PathVariable(name = "orderCode") String orderCode) {
+        if(loginService.getUser(sessionID) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        orderService.deleteOrder(orderCode, loginService.getUser(sessionID).getId());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @ApiOperation(value = "Remove an item from order")
+    @RequestMapping(value = "/orders/{orderCode}/items/{productCode}", method = RequestMethod.DELETE)
+    public ResponseEntity removeItemFromOrder(@PathVariable(name = "orderCode") String orderCode,
+                                              @PathVariable(name = "productCode") String productCode,
+                                              @RequestHeader("sessionID") UUID sessionID) {
+        if(loginService.getUser(sessionID) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        orderService.removeItemFromOrder(orderCode, productCode, loginService.getUser(sessionID).getId());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
+
+    @ApiOperation(value = "Add items to order")
+    @RequestMapping(value = "/orders/{orderCode}/items", method = RequestMethod.POST)
+    public ResponseEntity<Order> addItemsToOrder(@Valid @RequestBody ItemResource item,
+                                                 @PathVariable(name = "orderCode") String orderCode,
+                                                 @RequestHeader("sessionID") UUID sessionID) {
+
+        if(loginService.getUser(sessionID) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(orderService.addItemsToOrder(orderCode, item, loginService.getUser(sessionID).getId()));
+
+    }
+
+    @ApiOperation(value = "Pay the order")
+    @RequestMapping(value = "/orders/{orderCode}", method = RequestMethod.PUT)
+    public ResponseEntity<Order> payOrder(@PathVariable(name = "orderCode") String orderCode,
+                                          @RequestHeader("sessionID") UUID sessionID) {
+
+        if(loginService.getUser(sessionID) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(orderService.payOrder(orderCode, loginService.getUser(sessionID).getId()));
+    }
+
+    @ApiOperation(value = "Cancel order")
+    @RequestMapping(value = "/orders/{orderCode}/cancel", method = RequestMethod.PUT)
+    public ResponseEntity calncelOrder(@PathVariable(name = "orderCode") String orderCode,
+                                       @RequestHeader("sessionID") UUID sessionID) {
+
+        if(loginService.getUser(sessionID) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(orderService.cancelOrder(orderCode, loginService.getUser(sessionID).getId()));
+    }
 }
